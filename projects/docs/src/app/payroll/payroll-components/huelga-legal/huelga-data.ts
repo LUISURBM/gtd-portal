558ce6d141d4e3af4d602c35e12edcc3bf201c48
@KevinMorenoG <kevin.moreno@gtd.com.co>
2021-06-17 12:42:46 -0500

    KMM-add: Huelga api inttegrado

--- projects/docs/src/app/payroll/payroll-components/huelga-legal/huelga-data.ts
+++ projects/docs/src/app/payroll/payroll-components/huelga-legal/huelga-data.ts
@@ -3,7 +3,7 @@ import { ValuesCatalog } from '../../../srv/in-mem-data-service';
 import { formatToDate } from '../../../values-catalog';
 
 export interface Huelga {
-  id: number;
+  id: string;
   fechaInicio: Date;
   fechaFin: Date;
   cantidad: number;
@@ -11,121 +11,121 @@ export interface Huelga {
 
 export const huelgas: Huelga[] = [
   {
-    id: 1,
+    id: '1',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 2,
+    id: '2',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 3,
+    id: '3',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 4,
+    id: '4',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 5,
+    id: '5',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 6,
+    id: '6',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 7,
+    id: '7',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 8,
+    id: '8',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 9,
+    id: '9',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 10,
+    id: '10',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 11,
+    id: '11',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 12,
+    id: '12',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 13,
+    id: '13',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 14,
+    id: '14',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 15,
+    id: '15',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 16,
+    id: '16',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 17,
+    id: '17',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 18,
+    id: '18',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 19,
+    id: '19',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
   },
   {
-    id: 20,
+    id: '20',
     fechaInicio: formatToDate('5/17/2021'),
     fechaFin: formatToDate('5/17/2021'),
     cantidad: 1,
--- projects/docs/src/app/payroll/payroll-components/huelga-legal/huelga.component.ts
+++ projects/docs/src/app/payroll/payroll-components/huelga-legal/huelga.component.ts
@@ -1,11 +1,11 @@
 import { Basico } from './../basicos/basico-data';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of, Subscription } from 'rxjs';
 import { InMemService } from '../../../srv/in-mem-service';
 import { NgGtdDS } from '../../../types/common-types';
 import {
@@ -14,33 +14,118 @@ import {
   huelgas,
 } from './huelga-data';
 import { HuelgaFormComponent } from './huelga-form.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { HuelgasLegalesService } from '../../../srv/payroll/api/rest/huelgasLegales.service';
+import { switchMap } from 'rxjs/operators';
+import { ConfirmDialogComponent } from '../../../shared/dialog/confirm/confirm-dialog.component';
 
 @Component({
   selector: 'app-payroll-huelgas',
   templateUrl: './huelgas.component.html',
   styleUrls: ['./huelgas.component.css'],
 })
-export class HuelgasComponent implements OnInit, AfterViewInit {
-  dataSource$: BehaviorSubject<NgGtdDS>;
+export class HuelgasComponent implements OnInit, AfterViewInit, OnDestroy {
+  form: FormGroup;
+  dataSource$: BehaviorSubject<NgGtdDS> = new BehaviorSubject<NgGtdDS>({
+    datasource: new MatTableDataSource<Huelga>([]),
+    displayedColumns: displayedColumns,
+  });
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
   @ViewChild(MatSort) sort!: MatSort;
+  
+  readResponseError = (err: any) => {
+    console.log(err);
+  };
+
+  subscriptions: Subscription[] = [];
+
+  listado = (data: any) =>
+    this.huelgasAPISrv.listFindAllUsingGET46(data.devengadosId, 'events', true, {});
+  readResponseTList = (data: any, message?: string) => {
+    this.loading((data?.type ?? 1) * 25);
+    if (!data.body) return;
+    let newarray = data?.body?.bodyDto?.map?.((element: any) => {
+      var key,
+        keys = Object.keys(element);
+      var n = keys.length;
+      var newobj: any = {};
+      while (n--) {
+        key = keys[n];
+        if (key.toLowerCase().split('fecha').length > 1) {
+          element[key] =
+            /* formatDate(element[key], 'full', 'es-Co') */ new Date(
+              element[key]
+            );
+        }
+        newobj[`${key.charAt(0).toLowerCase()}${key.substr(1, key.length)}`] =
+          element[key];
+      }
+      return newobj;
+    });
+    console.log(newarray);
+    let datasource = new MatTableDataSource<Huelga>(newarray);
+    if (this.paginator) {
+      this.paginator._intl.itemsPerPageLabel = 'Ver';
+      this.paginator._intl.getRangeLabel = (
+        page: number,
+        pageSize: number,
+        length: number
+      ) => {
+        const pagesize = pageSize > length ? length : pageSize;
+        return `Página ${page + 1}`;
+      };
+    }
+    datasource.paginator = this.paginator;
+    datasource.sort = this.sort;
+    this.dataSource$.next({
+      datasource: datasource,
+      displayedColumns: displayedColumns,
+      loading: 100,
+    });
+  };
 
   constructor(
     public memSrv: InMemService,
     public dialog: MatDialog,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    public formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private huelgasAPISrv: HuelgasLegalesService
   ) {
-
-    this.dataSource$ = new BehaviorSubject<NgGtdDS>({
-      datasource: new MatTableDataSource<Huelga>(huelgas),
-      displayedColumns: displayedColumns,
+    this.form = this.formBuilder.group({
+      filtro: '',
+      fechaCorte: new Date(),
+      nominaGeneralId: undefined,
+      devengadosId: undefined,
     });
+    this.subscriptions = [
+      this.form.valueChanges
+        .pipe(
+          switchMap((data) => {
+            return this.listado(data);
+          })
+        )
+        .subscribe({
+          next: this.readResponseTList,
+          complete: this.loading,
+          error: this.readResponseError,
+        }),
 
-
+      this.route.params.subscribe((params) => {
+        const data = JSON.parse(params['data']);
+        console.log(data);
+        this.form.patchValue(data);
+      }),
+    ];
   }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
 
   ngOnInit(): void {}
 
@@ -51,33 +136,121 @@ export class HuelgasComponent implements OnInit, AfterViewInit {
     this.dataSource$.next({ ...this.dataSource$.value, datasource: datasource });
   }
 
-  add(name: Huelga): void {
-
-    if (!name) {
+  add(huelga: Huelga): void {
+    if (!huelga) {
       return;
     }
-    let datasource = this.dataSource$.value.datasource;
-    datasource.data = [
-      ...datasource.data,
-      { ...name, id: this.memSrv.genId(datasource.data, 'huelgas') },
-    ];
-    this.dataSource$.next({ ...this.dataSource$.value, datasource: datasource });
-  }
 
-  delete(libranza: Huelga): void {
-    let datasource = this.dataSource$.value.datasource;
-    datasource.data = datasource.data.filter((h) => h.id !== libranza.id);
-    this.dataSource$.next({ ...this.dataSource$.value, datasource: datasource });
-    this._snackBar.open(`${libranza.id}`, 'deleted!', { duration: 5000 });
-  }
+    const request = {
+      entidad: {
+        cantidad: huelga.cantidad,
+        fechaFin: huelga.fechaFin.toISOString(),
+        fechaInicio: huelga.fechaInicio.toISOString(),
+        businessSubscriptionId: '5B067D71-9EC0-4910-8D53-018850FDED4E',
+        enabled: true,
+        eventDate: new Date().toDateString(),
+        eventType: 'CREATE',
+        eventUser: 'LFUM',
+        removed: false,
+        cuneNov: '',
+        novedad: false,
+      },
+      headerRequest: {
+        cliente: 'FF841F95-5FDC-4879-A6BD-EE8C93A82943',
+      },
+    };
+    this.subscriptions.push(
+      this.huelgasAPISrv
+        .saveUsingPOST51(request, 'events', true, {
+          httpHeaderAccept: 'application/json',
+        })
+        .pipe(
+          switchMap((response: any) => {
+            if (!(response.type === 4)) return of();
+            if (response.type === 4 && response.status == 200)
+              this._snackBar.open(`Huelga ${huelga.fechaInicio.toISOString()}`, 'creada!', {
+                duration: 500000,
+              });
 
-  edit(libranza: Huelga): void {
-    let datasource = this.dataSource$.value.datasource;
-    const editedData = datasource.data.map((h) =>
-      h.id !== libranza.id ? h : libranza
+            return this.listado(response);
+          })
+        )
+        .subscribe({
+          next: this.readResponseTList,
+          error: this.readResponseError,
+        })
     );
-    datasource.data = editedData;
-    this.dataSource$.next({ ...this.dataSource$.value, datasource: datasource });
+  }
+
+  delete(huelga: Huelga): void {
+    this.subscriptions.push(
+      this.confirm(`¿Eliminar huelga ${huelga.fechaInicio.toISOString()}!?`)
+        .pipe(
+          switchMap((confirmacion) =>
+            confirmacion
+              ? this.huelgasAPISrv.deleteUsingDELETE51(
+                  huelga.id,
+                  'events',
+                  true,
+                  {
+                    httpHeaderAccept: 'application/json',
+                  }
+                )
+              : of()
+          ),
+          switchMap((data: any) =>
+            data.type === 4 && data.status === 200
+              ? this.listado(this.form.value)
+              : of()
+          )
+        )
+        .subscribe({
+          next: (data: any) => this.readResponseTList(data, 'eliminada!'),
+          error: (err: any) => {
+            console.log(err);
+          },
+        })
+    );
+  }
+
+  edit(huelga: Huelga): void {
+    const request = {
+      entidad: {
+        cantidad: huelga.cantidad,
+        fechaFin: huelga.fechaFin.toISOString(),
+        fechaInicio: huelga.fechaInicio.toISOString(),
+        id: huelga.id,
+        businessSubscriptionId: '5B067D71-9EC0-4910-8D53-018850FDED4E',
+        enabled: true,
+        eventDate: new Date().toISOString(),
+        eventType: 'CREATE',
+        eventUser: 'LFUM',
+        removed: false,
+      },
+      headerRequest: {
+        cliente: 'FF841F95-5FDC-4879-A6BD-EE8C93A82943',
+      },
+    };
+
+    this.subscriptions.push(
+      this.huelgasAPISrv
+        .updateUsingPUT51(request, 'events', true, {
+          httpHeaderAccept: 'application/json',
+        })
+        .pipe(
+          switchMap((response: any) => {
+            this._snackBar.open(`Huelga ${huelga.fechaInicio.toISOString()}`, 'actualizado!', {
+              duration: 500000,
+            });
+            return this.listado(response);
+          })
+        )
+        .subscribe({
+          next: this.readResponseTList,
+          complete: this.loading,
+          error: this.readResponseError,
+        })
+    );
   }
 
   applyFilter(event: Event) {
@@ -105,5 +278,20 @@ export class HuelgasComponent implements OnInit, AfterViewInit {
       if (result?.id) this.edit(result);
       else this.add(result);
     });
+  }
+  
+  loading = (loading = 100) =>
+    this.dataSource$.next({ ...this.dataSource$.value, loading: loading });
+
+  confirm(pregunta: string, titulo?: string) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '350px',
+      data: {
+        titulo: titulo,
+        pregunta: pregunta,
+      },
+    });
+
+    return dialogRef.afterClosed();
   }
 }
